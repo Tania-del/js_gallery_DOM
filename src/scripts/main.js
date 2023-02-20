34f7bfe8f7dc97dfd989737b1adfc0e1a9d069b3
Moroz Tetiana <tania@Morozs-MacBook-Air.local>
2023-02-20 16:25:59 +0200

    changed image by click

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const gallery = document.querySelectorAll('a');
+const currentImg = document.querySelector('.gallery__img');
+
+for (let i = 0; i < gallery.length; i++) {
+  const currentValue = gallery[i];
+
+  currentValue.addEventListener('click', (e) => {
+    currentImg.src = e.target.src;
+    e.preventDefault();
+  });
+}
